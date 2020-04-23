2f9a77294e9d357255efc062af3b7cb949468819
idin <py@idin.ca>
2020-04-23 10:32:39 -0700

    add convert_pdf_to_text

---
+++ pdf/convert_pdf_to_text.py
@@ -0,0 +1,23 @@
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LTTextBoxHorizontal
+
+
+def convert_pdf_to_text(document):
+    # convert all horizontal text into a lines list (one entry per line)
+    # document is a file stream
+    lines = []
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.get_pages(document):
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for element in layout:
+                if isinstance(element, LTTextBoxHorizontal):
+                    lines.extend(element.get_text().splitlines())
+    return lines
