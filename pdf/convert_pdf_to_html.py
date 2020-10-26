5676fe70fda2125af6cbe7d21303da21a0b7bb7e
idin <py@idin.ca>
2020-10-25 17:19:08 -0700

    small changes to convert_pdf_to_html

--- pdf/convert_pdf_to_html.py
+++ pdf/convert_pdf_to_html.py
@@ -5,7 +5,7 @@ import pdfminer.layout
 import pdfminer.high_level
 import sys
 from time import sleep
-from .exceptions import PdfError
+from .exceptions import ConversionError
 
 OUTPUT_TYPES = (
     (".htm", "html"),
@@ -27,7 +27,7 @@ def extract_text(
         files=None, outfile='-',
         no_laparams=False, all_texts=None, detect_vertical=None,
         word_margin=None, char_margin=None, line_margin=None,
-        boxes_flow=None, output_type='text', codec='utf-8',
+        boxes_flow=None, output_type='text', # codec='utf-8',
         strip_control=False, maxpages=0, page_numbers=None,
         password="", scale=1.0, rotation=0, layoutmode='normal',
         output_dir=None, debug=False, disable_caching=False,
@@ -80,21 +80,25 @@ def convert_pdf_to_html(pdf_path, html_path, num_tries=3):
         html_path = html_path.path
 
     for i in range(num_tries):
-        extract_text(
-            files=[pdf_path], debug=False, disable_caching=False, page_numbers=None, pagenos=None, maxpages=0,
-            password='', rotation=0, no_laparams=False, detect_vertical=False, char_margin=2.0,
-            word_margin=0.1, line_margin=0.5, boxes_flow=0.5, all_texts=True, outfile=html_path,
-            output_type='html', codec='utf-8',
-            output_dir=None, layoutmode='normal', scale=1.0, strip_control=False
-        )
-        if Path(html_path).exists():
-            break
-        else:
-            sleep(2 ** i)
+        try:
+            extract_text(
+                files=[pdf_path], debug=False, disable_caching=False, page_numbers=None, pagenos=None, maxpages=0,
+                password='', rotation=0, no_laparams=False, detect_vertical=False, char_margin=2.0,
+                word_margin=0.1, line_margin=0.5, boxes_flow=0.5, all_texts=True, outfile=html_path,
+                output_type='html', # codec='utf-8',
+                output_dir=None, layoutmode='normal', scale=1.0, strip_control=False
+            )
+            if Path(html_path).exists():
+                break
+            else:
+                sleep(2 ** i)
+        except Exception as e:
+            if i < 2:
+                continue
+            else:
+                raise ConversionError(f'error converting "{pdf_path}" to html: {e}')
     else:
-        error_message = f'error_converting_"{pdf_path}"_to_html'
+        error_message = f'error converting "{pdf_path}" to html'
         Path(html_path).save(obj=error_message)
-        raise PdfError(error_message)
-
-
-
+        raise ConversionError(error_message)
+    return html_path
